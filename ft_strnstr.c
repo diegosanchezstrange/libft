1cdcf1494a818379b5916d3446bd06a1f6b50f37
diegosanchezstrange <diegosanchezstrangejavax@gmail.com>
2021-09-08 15:47:39 +0200

    finished first part

---
+++ ft_strnstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsanchez <dsanchez@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/05 22:40:03 by dsanchez          #+#    #+#             */
+/*   Updated: 2021/09/05 23:28:01 by dsanchez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "string.h"
+#include "stdio.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	ll;
+	size_t	lb;
+
+	i = 0;
+	ll = ft_strlen(little);
+	lb = ft_strlen(big);
+	if (!ll)
+		return ((char *)big);
+	while (i < len && i + ll <= lb)
+	{
+		if (big[i] == little[0] 
+				&& ft_strncmp(((char *)little), ((char *)big) + i, ll) == 0)
+			return (((char *)big) + i);
+		i++;
+	}
+	return (NULL);
+}
+
--- libft.h
+++ libft.h
@@ -1,6 +1,7 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <string.h>
+# include <stdlib.h>
 
 size_t			ft_strlen(char const *s);
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
@@ -8,6 +9,7 @@ int				ft_strncmp(char *s1, char *s2, unsigned int n);
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
 char			*ft_strchr(const char *s, int c);
 char			*ft_strrchr(const char *s, int c);
+char			*ft_strnstr(const char *big, const char *little, size_t len);
 void 			*ft_memset(void *s, int c, size_t n);
 void 			ft_bzero(void *s, size_t n);
 void			*ft_memcpy(void *dest, const void * src, size_t n);
@@ -22,5 +24,8 @@ int				ft_isascii(int c);
 int				ft_isprint(int c);
 int				ft_toupper(int c);
 int				ft_tolower(int c);
+int				ft_atoi(const char *nptr);
+void			*ft_calloc(size_t nmemb, size_t size);
+char			*ft_strdup(const char *s);
 
 #endif
