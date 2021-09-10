0e006e32c0ab932a840c1c66140e06720cbae8b2
diegosanchezstrange <diegosanchezstrangejavax@gmail.com>
2021-09-10 18:20:54 +0200

    join done

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -11,8 +11,31 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	return (NULL);
+	char	*res;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	i = 0;
+	j = 0;
+	if (!(res = (char *) malloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
+		return (NULL);
+	while (s1[i])
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		res[i] = s2[j];
+		i++;	
+		j++;
+	}
+	res[i] = 0;
+	return (res);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -11,8 +11,37 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	int		len;
+	int		start;
+	int		i;
+	char	*res;
+
+	len = -1;
+	start = -1;
+	i = 0;
+	if (!s1 || !set)
+		return (NULL);
+	while (s1[i] && len != -1)
+	{
+		if(start == -1 && ft_strncmp((char *)s1 + i, (char *)set, ft_strlen(set)))
+			start = i;
+		else if (start != -1 && len == -1 
+				&& !ft_strncmp((char *)s1 + i, (char *)set, ft_strlen(set)) 
+				&& ft_strlen(s1) - i == ft_strlen(set))
+			len = i - start;
+		i++;
+	}
+	i = 0;
+	if (!(res = (char *)malloc(len + 1)))
+		return (NULL);
+	while (i < len)
+	{
+		res[i] = s1[start + i];
+		i++;
+	}
 	return (NULL);
 }
